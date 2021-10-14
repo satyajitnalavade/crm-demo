0c426ad261577895742ac828c7763fe599833231
satyajitn <satyajit.nalavade@gmail.com>
2021-10-13 20:35:08 -0500

    added gitignore

--- src/main/java/com/example/crm/backend/repository/CompanyRepository.java
+++ src/main/java/com/example/crm/backend/repository/CompanyRepository.java
@@ -2,9 +2,11 @@ package com.example.crm.backend.repository;
 
 import com.example.crm.backend.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author satya
  */
+@Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> {
 }
--- src/main/java/com/example/crm/backend/repository/ContactRepository.java
+++ src/main/java/com/example/crm/backend/repository/ContactRepository.java
@@ -1,16 +1,17 @@
 package com.example.crm.backend.repository;
 
 import com.example.crm.backend.entity.Contact;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * @author satya
  */
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
     @Query("select c from Contact c " +
